8d737d735baaf7d5a98339572838a4ce96d6625e
chris martinex <uschrismartinez@gmail.com>
2022-12-09 19:13:28 -0800

    new problem added, and some pseudeo code

--- applica.js
+++ applica.js
@@ -85,3 +85,41 @@ if (day === "work day")
   console.log("enjoy your holiday!");
 }
 console.log("christmans");
+
+// ADD CODE HERE
+// write a for loop 
+// push each number into fb
+// string "fizz" divisible by 3
+//string "buzz" divisible by 5
+// string "fizzbuzz" divisible by 3 and 5
+// i understood this problem but I'm having to re go over it beacuse it seems confussing, that in the original problem
+//when you psudeo code it they have the information where fizzbuzz is further down the message, but I'm having to put it on top
+//of my for loop when i though I would work on problem even when read from top to bottom, but I guess not. 
+const fb = [];
+
+// ADD CODE HERE
+let fb = [];
+// create for loop 1 through 16
+for (let i = 1; i < 17; i ++)
+  {
+ if (i % 3 === 0 && i % 5 === 0){
+      fb.push("fizzbuzz");
+    }else if (i % 3 === 0)
+    {
+      fb.push("fizz");
+    }else if (i % 5 === 0)
+      {
+        fb.push("buzz");
+      }else{
+        fb.push(i);
+      }
+  
+  }
+console.log(fb);
+// should log: [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16]
+//string "buzz" divisible by 5
+// string "fizzbuzz" divisible by 3 and 5
+    
+
+
+// should log: [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16]
